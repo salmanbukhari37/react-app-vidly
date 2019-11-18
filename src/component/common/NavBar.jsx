96b905f87814238e0e2b52f8b2d8def9fd5dd45b
Salman Bukhari <salmanbukhari37@gmail.com>
2019-11-18 22:12:48 +0500

    css and other files updated

--- src/component/common/NavBar.jsx
+++ src/component/common/NavBar.jsx
@@ -31,10 +31,6 @@ const NavBar = () => {
             <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
             </li>
         </ul>
-        <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
         </div>
     </nav>
     );
